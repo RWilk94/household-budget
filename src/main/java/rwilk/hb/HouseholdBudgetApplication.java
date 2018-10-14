e4b31bff5c520ae55fdf2ec56e00cd2a7eb3766b
Rafał Wilk <wilkrafal15@gmail.com>
2018-10-14 15:13:26 +0200

    Create backend project

---
+++ src/main/java/rwilk/hb/HouseholdBudgetApplication.java
@@ -0,0 +1,12 @@
+package rwilk.hb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseholdBudgetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseholdBudgetApplication.class, args);
+    }
+}
