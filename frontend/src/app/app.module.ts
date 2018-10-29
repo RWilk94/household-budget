4e3559611d85af61ec5a539c22b9642ee9f6bb93
Rafał Wilk <wilkrafal15@gmail.com>
2018-10-29 14:30:42 +0100

    Configure material design for bootstrap.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import {NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {RegistrationComponent} from './components/registration/registration.component';
 import {AppRoutingModule} from "./app-routing.module";
-import { StartComponent } from './components/start/start.component';
-import { LoginComponent } from './components/login/login.component';
+import {StartComponent} from './components/start/start.component';
+import {LoginComponent} from './components/login/login.component';
+import {FormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MDBBootstrapModule} from "angular-bootstrap-md";
 
 @NgModule({
   declarations: [
@@ -16,9 +19,14 @@ import { LoginComponent } from './components/login/login.component';
   ],
   imports: [
     AppRoutingModule,
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MDBBootstrapModule.forRoot(),
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [NO_ERRORS_SCHEMA]
 })
-export class AppModule { }
+export class AppModule {
+}
