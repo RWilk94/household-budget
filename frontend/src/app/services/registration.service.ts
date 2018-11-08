5d8389964a7536a04467344070586464ddeca1d3
Rafał Wilk <wilkrafal15@gmail.com>
2018-11-08 15:15:49 +0100

    Update registration

--- frontend/src/app/services/registration.service.ts
+++ frontend/src/app/services/registration.service.ts
@@ -8,19 +8,22 @@ import {User} from "../models/user";
 export class RegistrationService {
 
   private header = new HttpHeaders({'Content-Type': 'application/json'});
+  private link = 'https://rwilk-household-budget.cfapps.io';
 
   constructor(private http: HttpClient) {
   }
 
   register(user: User) {
     console.log('register: ' + JSON.stringify(user));
-    let url = 'http://localhost:8888/registration';
+    // let url = 'http://localhost:8888/registration';
+    let url = this.link + '/registration';
     return this.http.post(url, user, {headers: this.header});
   }
 
   login(user: User) {
     console.log('login: ' + JSON.stringify(user));
-    let url = 'http://localhost:8888/login';
+    // let url = 'http://localhost:8888/login';
+    let url = this.link + '/login';
     return this.http.post(url, user, {headers: this.header});
   }
 
