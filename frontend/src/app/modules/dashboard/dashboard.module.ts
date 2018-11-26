2b8f2a037368a6df6ef8de8b82911b71ac940bea
Rafał Wilk <rwilk@corelogic.com>
2018-11-26 12:09:53 +0100

    Update imports

--- frontend/src/app/modules/dashboard/dashboard.module.ts
+++ frontend/src/app/modules/dashboard/dashboard.module.ts
@@ -2,13 +2,11 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {DashboardComponent} from './components/dashboard/dashboard.component';
 import {DashboardRoutingModule} from "./dashboard-routing.module";
-import {SharedModule} from "../shared/shared.module";
 import {TestComponent} from './components/test/test.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    SharedModule,
     DashboardRoutingModule
   ],
   providers: [],
