51b369fe771da5bd1cd3dbc67121447fb601a121
Rafał Wilk <rwilk@corelogic.com>
2018-11-28 15:06:05 +0100

    Update exception handler

--- src/main/java/rwilk/hb/rest/exception/RestExceptionHandler.java
+++ src/main/java/rwilk/hb/rest/exception/RestExceptionHandler.java
@@ -1,16 +1,26 @@
 package rwilk.hb.rest.exception;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
+  /**
+   * Catch MethodArgumentNotValidException when request has invalid format of parameters in body.
+   * @param exception
+   * @param headers
+   * @param status
+   * @param request
+   * @return
+   */
   @Override
   protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception,
       HttpHeaders headers, HttpStatus status, WebRequest request) {
@@ -22,6 +32,23 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         .details(exception.getBindingResult().toString())
         .build();
 
+    return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+  }
+
+  /**
+   * Catch SQLIntegrityConstraintViolationException when try save duplicated key.
+   * @param exception
+   * @return
+   */
+  @ExceptionHandler
+  public ResponseEntity<Object> handleConstraintViolationException(DataIntegrityViolationException exception) {
+
+    ErrorResponse response = ErrorResponse.builder()
+        .status(HttpStatus.BAD_REQUEST.value())
+        .message("Record already exists.")
+        .timeStamp(System.currentTimeMillis())
+        .details(exception.getCause().toString())
+        .build();
     return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
   }
 
