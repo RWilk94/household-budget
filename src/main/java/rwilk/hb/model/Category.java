889f2b8d1633a63c41df20be20f5d4f03d074197
Rafał Wilk <rwilk@corelogic.com>
2018-11-30 12:06:19 +0100

    Update configuration

--- src/main/java/rwilk/hb/model/Category.java
+++ src/main/java/rwilk/hb/model/Category.java
@@ -17,7 +17,6 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
