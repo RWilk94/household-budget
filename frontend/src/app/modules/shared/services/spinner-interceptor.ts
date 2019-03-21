1cbc23fec0b7871283f2fc00fc4936632dc5c004
Rafał Wilk <rwilk@corelogic.com>
2019-03-21 12:41:08 +0100

    Refactor spinner interceptor

--- frontend/src/app/modules/shared/services/spinner-interceptor.ts
+++ frontend/src/app/modules/shared/services/spinner-interceptor.ts
@@ -1,20 +1,29 @@
 
 import {throwError as observableThrowError, BehaviorSubject, Observable, Subject, throwError} from 'rxjs';
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
-import {catchError, finalize, map} from "rxjs/operators";
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse} from "@angular/common/http";
+import {catchError, finalize, map, tap} from "rxjs/operators";
 import {Injectable} from "@angular/core";
 
 @Injectable()
 export class HTTPStatusService {
 
   private requestInFlight$: BehaviorSubject<boolean>;
+  private requests = 0;
 
   constructor() {
     this.requestInFlight$ = new BehaviorSubject(false);
   }
 
   setHttpStatus(inFlight: boolean) {
-    this.requestInFlight$.next(inFlight);
+    if (inFlight === true) {
+      this.requests += 1;
+      this.requestInFlight$.next(inFlight);
+    } else {
+      this.requests -= 1;
+      if (this.requests === 0) {
+        this.requestInFlight$.next(inFlight);
+      }
+    }
   }
 
   getHttpStatus(): Observable<boolean> {
@@ -31,9 +40,13 @@ export class SpinnerInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     this.status.setHttpStatus(true);
-    return next.handle(request).pipe(
-      finalize(() => this.status.setHttpStatus(false))
-    );
+    return next.handle(request).pipe(tap((event: HttpEvent<any>) => {
+      if (event instanceof HttpResponse) {
+        this.status.setHttpStatus(false);
+      }
+    }, (error: any) => {
+      this.status.setHttpStatus(false);
+    }));
   }
 
 }
